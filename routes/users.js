0607febe3ce1bb2c88ae47d517c2a3479f5ca204
Kaung Win <tkong7511@yahoo.com>
2021-02-13 06:33:25 -0800

    Mongoose Population

--- routes/users.js
+++ routes/users.js
@@ -14,20 +14,31 @@ router.post('/signup', (req, res) => {
     User.register(
         new User({username: req.body.username}),
         req.body.password,
-        err => {
+        (err, user)=> {
             if (err) {
                 res.statusCode = 500;
                 res.setHeader('Content-Type', 'application/json');
                 res.json({err: err});
             } else {
-                passport.authenticate('local')(req, res, () => {
-                    res.statusCode = 200;
-                    res.setHeader('Conten-Type', 'application/json');
-                    res.json({success: true, status: 'Registration Successful'});
+                if (req.body.firstname) {
+                    user.firstname = req.body.firstname;
+                }
+                user.save(err => {
+                    if (err) {
+                        res.statusCode = 500;
+                        res.setHeader('Content-Type', 'application/json');
+                        res.json({err: err});
+                        return;
+                    }
+                    passport.authenticate('local')(req, res, () => {
+                        res.statusCode = 200;
+                        res.setHeader('Conten-Type', 'application/json');
+                        res.json({success: true, status: 'Registration Successful'});
+                    });
                 });
             }
         }
-    )
+    );
 });
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
